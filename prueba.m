427655ae5a3cd947a9ac6054c53f3fc2d0ae30e1
Lacho_t <82452750+lacho22@users.noreply.github.com>
2021-09-28 16:08:13 -0500

    Demostración carga de archivo
    tercera demostración sobre como subir archivos al repo

---
+++ prueba.m
@@ -0,0 +1,16 @@
+% Script Octave
+% Title 	:Prueba 2 de octave
+% Description	:Segunda prueba de edición y ejecuación de script octave
+% Author	:Ing. Horacio RoVe(lacho_t)
+% Date		:20210928
+% Version	:1
+% Usage		:octave> cd /path
+%		:octave>prueba
+% Notes		:Requiere CLI Octave
+
+clear
+a=3
+b=5
+c=7
+a+(b+c)
+(a+b)+c
