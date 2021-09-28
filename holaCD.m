d3942e8db073b6f04eb87ae16876886e881aef5c
Lacho_t <82452750+lacho22@users.noreply.github.com>
2021-09-28 10:39:45 -0500

    Demostración de carga de archivos
    Incorporación de nuevos scripts para fines demostrativos

---
+++ holaCD.m
@@ -0,0 +1,17 @@
+% Octave Script
+% Title			:Hola Mundo Octave
+% Description	:Script para conocer octave
+% Author		:Ing. Horacio RoVe (lacho_t) 
+% Date			:20210928
+% Version		:1
+% Usage			:octave> /path/HolaCD
+% Notes			:Requiere aplicación octave, usar su línea de comandos
+
+clear
+% a + b ∈ ℝ, a * b ∈ ℝ propiedad de cerrradura
+a=3;
+b=5;
+a+b
+a*b
+
+% 
